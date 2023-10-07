7cd50febac7147b3c272c191d9668fcb97e95681
Alexander Melekhin <amelekhin96@gmail.com>
2023-10-07 13:25:11 +0300

    fix: change deprecated 'pretrained' parameter to 'weights'

--- cats_vs_dogs/models.py
+++ cats_vs_dogs/models.py
@@ -1,9 +1,9 @@
 from torch import nn
-from torchvision import models
+from torchvision.models import ResNet18_Weights, resnet18
 
 
 def load_resnet18(num_classes: int, freeze_backbone: bool = True) -> nn.Module:
-    model = models.resnet18(pretrained=True)
+    model = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
     num_feats = model.fc.in_features
     model.fc = nn.Linear(num_feats, num_classes)
     if freeze_backbone:
